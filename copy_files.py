2615f07b1286617d24c1c36cea141a308f3002c7
Maple <wang-jiaxun@outlook.com>
2024-09-29 15:20:33 +0800

    add cur preview (via ico)

---
+++ copy_files.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+CURRENT = Path(__file__).resolve().parent
+
+CURRENT.joinpath("Cursors_ico").mkdir(exist_ok=True)
+files = list(CURRENT.joinpath("Cursors").glob("*.cur"))
+
+for file in files:
+    dest = CURRENT.joinpath("Cursors_ico")
+    import shutil
+
+    shutil.copy(file, dest.joinpath(file.stem + ".ico"))
