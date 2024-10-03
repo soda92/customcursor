98a978185f3fa4380955a716294479f245b5adb0
Maple <wang-jiaxun@outlook.com>
2024-10-03 22:56:36 +0800

    add precise value for hotspot

--- new_cursor.py
+++ new_cursor.py
@@ -3,6 +3,20 @@ from pathlib import Path
 file = Path("aero_link.ico")
 data = bytearray(file.read_bytes())
 data[10] = 102
+data[11] = 0
+data[12] = 0
+data[13] = 0
 data[2] = 0x02
 
 Path("aero_link.cur").write_bytes(data)
+
+
+file = Path("221.ico")
+data = bytearray(file.read_bytes())
+data[10] = 127
+data[11] = 0
+data[12] = 0
+data[13] = 0
+data[2] = 0x02
+
+Path("33.cur").write_bytes(data)
